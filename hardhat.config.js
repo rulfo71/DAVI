7a3ce7f9caf627f947dadd520682cce4b4ad6a87
Tomas Pulenta <tpulenta@gmail.com>
2022-11-17 08:15:26 -0300

    upgrade package.json, yarn upgrade and updating hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -106,6 +106,15 @@ module.exports = {
           },
         },
       },
+      {
+        version: '0.8.17',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
     ],
   },
   networks: {
